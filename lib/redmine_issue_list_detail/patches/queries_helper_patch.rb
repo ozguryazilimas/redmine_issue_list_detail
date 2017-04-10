b55946875943360f08870d5dd23863de48e2824c
Onur Küçük <onur@delipenguen.net>
2017-04-10 17:31:17 +0300

    group link builder within same block

--- lib/redmine_issue_list_detail/patches/queries_helper_patch.rb
+++ lib/redmine_issue_list_detail/patches/queries_helper_patch.rb
@@ -22,18 +22,14 @@ module RedmineIssueListDetail
           return column_value_without_redmine_issue_list_detail(column, issue, value) unless column.name == :relations
 
           content_tag('span',
-            value.to_s(issue) {|other| rild_link_to_issue_with_status(other)}.html_safe,
+            value.to_s(issue) do |other|
+              text = "##{other.id} (#{other.status})"
+              title = other.subject.truncate(60)
+              link_to(text, issue_url(issue, :only_path => true), :class => issue.css_classes, :title => title)
+            end.html_safe,
             :class => value.css_classes_for(issue))
         end
 
-        private
-
-        def rild_link_to_issue_with_status(issue)
-          text = "##{issue.id} (#{issue.status})"
-          title = issue.subject.truncate(60)
-
-          link_to(text, issue_url(issue, :only_path => true), :class => issue.css_classes, :title => title)
-        end
       end
 
     end
